d8057c117cdec136b747f27c0b06eba260ba0872
GioBo <boccigionata@gmail.com>
2014-10-10 11:35:58 +0200

    fixed BiolFlor traits

--- R/biolflor.R
+++ R/biolflor.R
@@ -9,53 +9,6 @@ list_of_traits_Biolflor=c(
     ,"Type of seed production"                
     ,"Strategy type"                          
     ,"Pollen vector"
-
-                                        #"Floristic status"                       
-                                        #"Evolutionary origin"                    ,
-                                        # "Region of origin"                       ,
-                                        # "Endemism"                               ,
-                                        # "Mode of introduction"                   ,
-                                        # "Time and region of first naturalization",
-                                        # "Degree of naturalization"               ,
-                                        # "Chromosome number"                      ,
-                                        # "DNA content"                            ,
-                                        # "Phylogeny code"                         ,
-                                        # "Shoot metamorphoses"                    ,
-                                        # "Root metamorphoses"                     ,
-                                        # "Climbing"                               ,
-                                        # "Nutritional adaptations"                ,
-                                        # "Leaf persistence"                       ,
-                                        # "Leaf anatomy"                           ,
-                                        # "Leaf form"                              ,
-                                        # "Symphenological Groups"                 ,
-                                        # "Type of apomixis"                       ,
-                                        # "Dicliny"                                ,
-                                        # "Dichogamy"                              ,
-                                        # "Heterostyly"                            ,
-                                        # "Self-sterility and self-incompatibility",
-                                        # "SI mechanism"                           ,
-                                        # "Breeding system"                        ,
-                                        # "P/O ratio"                              ,
-                                        # "Floral rewards"                         ,
-                                        # "Flower color"                           ,
-                                        # "UV reflection of the flower"            ,
-                                        # "Flower class after MUELLER"             ,
-                                        # "Flower type after KUGLER"               ,
-                                        # "Fruit type"                             ,
-                                        # "Diaspore type"                          ,
-                                        # "Germinule type"                         ,
-                                        # "Weights"                                ,
-                                        # "Sizes"                                  ,
-                                        # "Grassland utilization indicator values" ,
-                                        # "Floristic zone and altitudinal level"   ,
-                                        # "Number of inhabited floristic zones"    ,
-                                        # "Oceanity"                               ,
-                                        # "Floristic region"                       ,
-                                        # "Hemeroby"                               ,
-                                        # "Number of hemerobic levels"             ,
-                                        # "Urbanity"                               ,
-                                        # "Habitat"                                ,
-                                        # "Class"                                  
     )
 
 traits_special_Biolflor=c(
