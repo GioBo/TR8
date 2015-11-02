22cff3b6bd5ec1632115a3fbd91cdbc2b3a9914b
GioBo <boccigionata@gmail.com>
2015-11-02 13:58:30 +0100

    added required BiolFlor traits

--- data/traits_Biolflor.R
+++ data/traits_Biolflor.R
@@ -2,25 +2,36 @@
 
 list_of_traits_Biolflor=c(
     "Life form"                        
-    ,"Life span"                              
-    ,"Rosettes"                               
-    ,"Vegetative propagation"                 
-    ,"Storage organs"                         
-    ,"Type of reproduction"                   
-    ,"Type of seed production"                
-    ,"Strategy type"                          
-    ,"Pollen vector"
-    )
+   ,"Life span"                              
+   ,"Rosettes"                               
+   ,"Vegetative propagation"                 
+   ,"Storage organs"                         
+   ,"Type of reproduction"                   
+   ,"Type of seed production"                
+   ,"Strategy type"                          
+   ,"Pollen vector"
+   ,"Self-sterility and self-incompatibility"
+   ,"Breeding system"
+
+    
+)
 
 ### * List of special traits to download from BiolFlor
 
-## traits_special_Biolflor=c(
-##     "Flower class after MUELLER",
-##     "Begin of flowering (months)",
-##     "End of flowering (months)",
-##     "Duration of flowering (months)",
-##     "Number of flowering phases"
-## )
+traits_special_Biolflor=c(
+    "Flower class after MUELLER"
+        ,"Type of apomixis"
+    ## "Begin of flowering (months)",
+    ## "End of flowering (months)",
+    ## "Duration of flowering (months)",
+    ## "Number of flowering phases",
+    ## "Flower colour",
+    ## "UV reflection in the periphery of the flower",
+    ## "Floral rewards",
+    ## "Hemeroby",
+    ## "Urbanity",
+    ## "English circumscription"
+)
 
 
 ## traits_pollen_Biolflor=c("Pollen vector")
