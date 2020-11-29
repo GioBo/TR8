d166f5ba472274f56f923089b10c09d5ae503812
GioBo <boccigionata@gmail.com>
2020-11-29 20:40:05 +0100

    Fixed Pignatti

--- R/leda_download.R
+++ R/leda_download.R
@@ -25,29 +25,48 @@
 ##' traits of Northwest European flora. Journal of Ecology 96:1266-1274. \samp{  http://www.leda-traitbase.org/LEDAportal/data_files.jsp}
 ##' @author   Gionata Bocci <boccigionata@@gmail.com>
 leda_download_to_local_directory<-function(directory){
-    ## gmessage(title="","Downloading LEDA files is a time-consuming activity, \nthus you are suggested to download the dataset once\nand store them in a local directory.\n\nYou will now be asked to choose such a directory.")
-    ##directory<-gfile(type="selectdir")
-    ## load the list containing the data (url, names, etc..) of the txt files
-    ## data(leda_lookup)
-    ## convert it to a dframe
-    env<-new.env(parent = parent.frame())
-    data(leda_lookup,envir=env)
-    leda_lookup<-get("leda_lookup",envir=env)
-    DF<-ldply(leda_lookup)
-    ## build the first dataframe
-    first<-leda_download(url=DF[1,2],skip_row=DF[1,3],column=DF[1,4],out_name=DF[1,5])
-    rearranged<-first
-    ## download all the other txt files and merge each one to the
-    ## previous one
-    for(i in 2:nrow(DF)){
-        temp<-leda_download(url=DF[i,2],skip_row=DF[i,3],column=DF[i,4],out_name=DF[i,5])
-        rearranged<-merge(rearranged,temp,by.x=0,by.y=0,all.x=TRUE,all.y=TRUE)
-        row.names(rearranged)<-rearranged$Row.names
-        rearranged<-rearranged[,2:ncol(rearranged)]
-    }
-    ## save the compleate dataset in a file called "leda_database.Rda"
-    ## in the directory chosen by the user
-    remove(list=c("leda_lookup"), envir = env)    
-    #save(file=file.path(directory,"leda_database.Rda"),rearranged)
-    save(file=file.path(directory,"leda_database.Rda"),rearranged)
+  ## gmessage(title="","Downloading LEDA files is a time-consuming activity, \nthus you are suggested to download the dataset once\nand store them in a local directory.\n\nYou will now be asked to choose such a directory.")
+  ##directory<-gfile(type="selectdir")
+  ## load the list containing the data (url, names, etc..) of the txt files
+  ## data(leda_lookup)
+  ## convert it to a dframe
+  env<-new.env(parent = parent.frame())
+  data(leda_lookup,envir=env)
+  leda_lookup<-get("leda_lookup",envir=env)
+  ## DF<-ldply(leda_lookup)
+  ## ## build the first dataframe
+  ## first<-leda_download(url=DF[1,2],skip_row=DF[1,3],column=DF[1,4],out_name=DF[1,5])
+  ## rearranged<-first
+  ## ## download all the other txt files and merge each one to the
+  ## ## previous one
+  ## for(i in 2:nrow(DF)){
+  ##   temp<-leda_download(url=DF[i,2],skip_row=DF[i,3],column=DF[i,4],out_name=DF[i,5])
+  ##   rearranged<-merge(rearranged,temp,by.x=0,by.y=0,all.x=TRUE,all.y=TRUE)
+  ##   row.names(rearranged)<-rearranged$Row.names
+  ##   rearranged<-rearranged[,2:ncol(rearranged)]
+  ## }
+
+  ## create a list of dataframes, one for each LEDA file;
+  ## each dataframe has 2 columns: one called "Species" and a second
+  ## one with the code name of the trait
+  tmp_leda <- lapply(leda_lookup, function(x){
+    ## print(x)
+    a<-leda_download(
+      URL=x[1],
+      skip_row=x[2],
+      column=x[3],
+      out_name=x[4]
+    )
+    return(a)
+  })
+  ## merge all dataframes in a single one
+  RES <- Reduce(function(dtf1, dtf2) merge(dtf1, dtf2, by = "Species", all.x = TRUE),
+                tmp_leda)
+
+  ## save the complete dataset in a file called "leda_database.Rda"
+  ## in the directory chosen by the user
+  remove(list=c("leda_lookup"), envir = env)    
+                                        #save(file=file.path(directory,"leda_database.Rda"),rearranged)
+  save(file=file.path(directory,"leda_database.Rda"),RES)
 }
+
--- R/tr8.R
+++ R/tr8.R
@@ -317,7 +317,7 @@ tr8<-function(species_list,download_list=NULL,gui_config=FALSE,synonyms=FALSE,ca
             ## names(reference_names)<-species_list
             ## species_list<-unique(as.vector(unlist(reference_names)))
 
-            warning("The synonyms option deprecated in current version of TR8")
+            warning("The synonyms option is deprecated in current version of TR8")
 
         }
         
@@ -338,9 +338,9 @@ tr8<-function(species_list,download_list=NULL,gui_config=FALSE,synonyms=FALSE,ca
         ## if at least one LEDA trait is needed
         local_leda<-file.path(directory,"leda_database.Rda")
         if(file.exists(local_leda)){
-            load(local_leda)}else{
+            rearranged<-get(load(local_leda))}else{
             if(length(traits_list$LEDA)>0){
-
+              
                 ## unfortunately nls() does not work on Windows, thus I think it's better to remove that
                 ## if(tryCatch(nsl("www.cran.r-project.org"), error =function(e){return(FALSE)},warning=function(w){return(FALSE)})==FALSE){
                 ##     stop("You neither have a working internet connection nor locally stored LEDA files.\n  Please re-run tr8() function when your internet connection is working.")
@@ -351,9 +351,9 @@ tr8<-function(species_list,download_list=NULL,gui_config=FALSE,synonyms=FALSE,ca
                 if(tryCatch(url.exists(url_leda), error =function(e){return(FALSE)},warning=function(w){return(FALSE)})==FALSE){
                     stop("\n\n LEDA website is probably down.\n Please re-run tr8() without selecting LEDA as a source of data \n (or re-try later).\n\n")
                 }
-                
+              
                 local_storage(db="LEDA",directory)
-                load(local_leda)
+                rearranged<-get(load(local_leda))
             }else{rearranged<-NULL}
         }
         leda_traits<-leda(species_list,TRAITS=traits_list$LEDA,rearranged=rearranged)
