ebef817abecf02e585cb49a2e5084fc803da69ec
GioBo <boccigionata@gmail.com>
2020-12-01 18:55:12 +0100

    Updated greeting message

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,16 @@
 .onAttach<-function(...){
     packageStartupMessage(
         "
-        \tPlease remember to always cite the appropriate sources of traits data (see ?bib).
+
+        Please remember to always cite the appropriate sources of traits data (see ?bib).
+        \n
+        Please note: in new releases of TR8 package, the tr8 function accepts the additional
+        parameter 'allow_persistent': setting it to TRUE, the function will download some
+        data in your filesystem to make future queries run faster. If set to FALSE, data will
+        be stored in a temporary directory. If not set, you will be prompted with a request
+        when running tr8.
+
+       See also vignette('TR8').
         ")
 
     }
